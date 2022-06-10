e73485ae919a90c9bcd59499ebf6418295aabd9d
Tahsin Tunan <tahsintunan@gmail.com>
2022-06-10 16:53:41 +0600

    getBlogpostById implemented

--- back/src/controllers/blogController.ts
+++ back/src/controllers/blogController.ts
@@ -11,7 +11,31 @@ const getAllBlogs = (req: Request, res: Response) => {
     });
 }
 
+const getBlogpost = (req: Request, res: Response) => {
+    // get a blogpost by it's post id (:postId)
+    const id = parseInt(req.params.postId);
+    const queryString = `SELECT * FROM "public".post WHERE id = $1;`;
+    const values = [id];
+    pool.query(queryString, values, (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
+}
+
+
+const createBlogpost = (req: Request, res: Response) => {
+    const {title, content} = req.body;
+    // INSERT INTO "public".post (user_id, content) VALUES (1, 'NEW2');
+    const queryString = `INSERT INTO "public".post (user_id, title, content) VALUES ($1, $2, $3)`;
+    const values = [title, content];
+    pool.query(queryString, values, (error, results) => {
+        if (error) throw error;
+        res.status(201).json({message: 'Blogpost created successfully'});
+    });
+}
+
 
 export { 
     getAllBlogs,
+    getBlogpost,
 };
--- back/src/routes/blogRouter.ts
+++ back/src/routes/blogRouter.ts
@@ -3,28 +3,17 @@ const router = Router();
 // import blogController from '../controllers/blogController';
 const blogController = require('../controllers/blogController');
 
-// router.get('/', (req, res) => {
-//     res.send('Hello World from blogRouter!');
-// });
-
-
-
-
-
-
-
-
-
 
 router.get('/', blogController.getAllBlogs);
+router.get('/posts/:postId', blogController.getBlogpost);
+
+router.delete('/posts/:postId', (req, res) => {
+    // delete post by id
+    // then, return to user/posts/home/previous page
+})
 
 router.post('/post', (req, res) => {
     // create post
-    // then, return post
-})
-
-router.get('/posts/:postId', (req, res) => {
-    // get post by id
     // then, return post
 })
 
@@ -32,28 +21,6 @@ router.put('/posts/:postId', (req, res) => {
     // update post by id
     // then, return post
 })
-
-router.delete('/posts/:postId', (req, res) => {
-    // delete post by id
-    // then, return to user/posts/home/previous page
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
