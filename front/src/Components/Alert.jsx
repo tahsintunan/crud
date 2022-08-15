90b320141192bf198df002b9c37307078b4ff048
Tahsin Tunan <tahsintunan@gmail.com>
2022-08-15 15:31:19 +0600

    client side code cleanup

--- front/src/Components/Alert.jsx
+++ front/src/Components/Alert.jsx
@@ -33,12 +33,3 @@ const Alert = (props) => {
 }
 
 export default Alert;
-
-{/* <div role="alert">
-    <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-        Danger
-    </div>
-    <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-        <p>Something not ideal might be happening.</p>
-    </div>
-</div> */}
